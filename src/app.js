cc5d137c9264060a844df2912524d8e4c02586fe
Elif Ertürk <elif.erturk@ideabilisim.com>
2021-10-25 10:42:36 +0300

    18 ekim

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("Merhaba kod")
+
+let dolarDun=9.20
+
+let dolarBugun=9.30
+console.log(dolarBugun)
+
+
+//array
+
+let konutKredileri=[12,"konut kredisi","emlak","kamu","özel"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++)
+{
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
